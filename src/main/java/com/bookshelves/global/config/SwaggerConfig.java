d4ca2c4fae7933f63376a7e348e96a9f12effc58
wldsmdl7 <wldmsdl5835@gmail.com>
2026-07-03 10:37:05 +0900

    chore(swagger): 스웨거 설정

---
+++ src/main/java/com/bookshelves/global/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.bookshelves.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI swagger() {
+        Info info = new Info().title("책장사이").description("UMC 10기 프로젝트, 책장사이 스웨거입니다.").version("0.0.1");
+
+        // JWT 토큰 헤더 방식
+        String securityScheme = "JWT TOKEN";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityScheme);
+
+        Components components = new Components()
+                .addSecuritySchemes(securityScheme, new SecurityScheme()
+                        .name(securityScheme)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("Bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(info)
+                .addServersItem(new Server().url("/"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
