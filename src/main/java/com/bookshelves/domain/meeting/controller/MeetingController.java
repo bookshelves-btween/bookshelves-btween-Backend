230ceb4f101ed2101fcccc6951dc5d1d0a768289
wldsmdl7 <wldmsdl5835@gmail.com>
2026-07-21 21:21:54 +0900

    docs(meeting): 모임 생성 API 스웨거 작성

--- src/main/java/com/bookshelves/domain/meeting/controller/MeetingController.java
+++ src/main/java/com/bookshelves/domain/meeting/controller/MeetingController.java
@@ -5,13 +5,10 @@ import com.bookshelves.domain.meeting.dto.response.MeetingCreateResDTO;
 import com.bookshelves.domain.meeting.exception.code.MeetingSuccessCode;
 import com.bookshelves.domain.meeting.service.MeetingCommandService;
 import com.bookshelves.global.apiPayload.ApiResponse;
-import com.bookshelves.global.apiPayload.code.GeneralErrorCode;
-import com.bookshelves.global.exception.ProjectException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,13 +23,7 @@ public class MeetingController implements MeetingControllerDocs {
   @Override
   @PostMapping("/api/v1/{isbn}/recruitment")
   public ResponseEntity<ApiResponse<MeetingCreateResDTO>> createMeeting(
-      @PathVariable String isbn,
-      @Valid @RequestBody MeetingCreateReqDTO request,
-      BindingResult bindingResult) {
-    if (bindingResult.hasErrors()) {
-      throw new ProjectException(GeneralErrorCode.BAD_REQUEST);
-    }
-
+      @PathVariable String isbn, @Valid @RequestBody MeetingCreateReqDTO request) {
     MeetingCreateResDTO response = meetingCommandService.createMeeting(isbn, request);
     return ResponseEntity.status(HttpStatus.CREATED)
         .body(ApiResponse.onSuccess(MeetingSuccessCode.MEETING_CREATED, response));
