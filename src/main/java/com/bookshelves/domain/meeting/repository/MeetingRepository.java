88e5db08e44ddc899349cf429d3fd6760c85ee86
wldsmdl7 <wldmsdl5835@gmail.com>
2026-07-23 08:00:33 +0900

    feat(meeting): 모임 검색 API 구현

--- src/main/java/com/bookshelves/domain/meeting/repository/MeetingRepository.java
+++ src/main/java/com/bookshelves/domain/meeting/repository/MeetingRepository.java
@@ -2,6 +2,8 @@ package com.bookshelves.domain.meeting.repository;
 
 import com.bookshelves.domain.meeting.entity.Meeting;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +11,7 @@ public interface MeetingRepository extends JpaRepository<Meeting, Long> {
 
   @EntityGraph(attributePaths = "book")
   Optional<Meeting> findWithBookById(Long id);
+
+  @EntityGraph(attributePaths = "book")
+  Page<Meeting> findByBookTitleContainingIgnoreCase(String title, Pageable pageable);
 }
