9529129bfa4539067d33be5b872f12f70097a71c
wldsmdl7 <wldmsdl5835@gmail.com>
2026-07-23 11:02:21 +0900

    fix(meeting): develop 충돌 해결

--- src/main/java/com/bookshelves/domain/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/bookshelves/domain/chat/repository/ChatRoomRepository.java
@@ -9,5 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
   Optional<ChatRoom> findByMeetingId(Long meetingId);
+
   List<ChatRoom> findAllByMeetingIdIn(Collection<Long> meetingIds);
 }
