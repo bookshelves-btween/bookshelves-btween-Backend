46f1204d8de8b639a12f573d28631632e5667317
wldsmdl7 <wldmsdl5835@gmail.com>
2026-07-23 16:19:35 +0900

    feat(meeting): 모임 참여 API 구현

--- src/main/java/com/bookshelves/domain/meeting/service/MeetingCommandService.java
+++ src/main/java/com/bookshelves/domain/meeting/service/MeetingCommandService.java
@@ -7,8 +7,17 @@ import com.bookshelves.domain.book.repository.BookRepository;
 import com.bookshelves.domain.meeting.converter.MeetingConverter;
 import com.bookshelves.domain.meeting.dto.request.MeetingCreateReqDTO;
 import com.bookshelves.domain.meeting.dto.response.MeetingCreateResDTO;
+import com.bookshelves.domain.meeting.dto.response.MeetingParticipationResDTO;
 import com.bookshelves.domain.meeting.entity.Meeting;
+import com.bookshelves.domain.meeting.entity.MeetingParticipant;
+import com.bookshelves.domain.meeting.enums.MeetingStatus;
+import com.bookshelves.domain.meeting.exception.MeetingException;
+import com.bookshelves.domain.meeting.exception.code.MeetingErrorCode;
+import com.bookshelves.domain.meeting.repository.MeetingParticipantRepository;
 import com.bookshelves.domain.meeting.repository.MeetingRepository;
+import com.bookshelves.domain.member.entity.Member;
+import com.bookshelves.domain.member.repository.MemberRepository;
+import com.bookshelves.global.security.AuthenticationFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,19 +29,44 @@ public class MeetingCommandService {
 
   private final BookRepository bookRepository;
   private final MeetingRepository meetingRepository;
+  private final MeetingParticipantRepository meetingParticipantRepository;
+  private final MemberRepository memberRepository;
+  private final AuthenticationFacade authenticationFacade;
 
   public MeetingCreateResDTO createMeeting(String isbn, MeetingCreateReqDTO request) {
 
     Book book =
-        bookRepository
-            .findByIsbn(isbn)
-            .orElseThrow(
-                () -> // TODO: 카카오 도서 API에서 조회한 도서 정보를 DB에 저장한 뒤 반환하도록 변경
-                new BookException(BookErrorCode.BOOK_NOT_FOUND));
+      bookRepository
+        .findByIsbn(isbn)
+        .orElseThrow(
+          () -> // TODO: 카카오 도서 API에서 조회한 도서 정보를 DB에 저장한 뒤 반환하도록 변경
+            new BookException(BookErrorCode.BOOK_NOT_FOUND));
 
     Meeting meeting = MeetingConverter.toEntity(book, request);
     Meeting savedMeeting = meetingRepository.save(meeting);
 
     return MeetingCreateResDTO.from(savedMeeting);
+  }
+
+  public MeetingParticipationResDTO participateMeeting(Long meetingId) {
+    Meeting meeting =
+      meetingRepository
+        .findById(meetingId)
+        .orElseThrow(() -> new MeetingException(MeetingErrorCode.MEETING_NOT_FOUND));
+    Long memberId = 1001L;
+
+    if (meetingParticipantRepository.existsByMeetingIdAndMemberId(meetingId, memberId)) {
+      throw new MeetingException(MeetingErrorCode.DUPLICATE_MEETING);
+    }
+    if (meeting.getStatus() != MeetingStatus.RECRUITING) {
+      throw new MeetingException(MeetingErrorCode.MEETING_RECRUITMENT_CLOSED);
+    }
+
+    Member member = memberRepository.getReferenceById(memberId);
+
+    MeetingParticipant meetingParticipant =
+      meetingParticipantRepository.save(MeetingParticipant.create(meeting, member));
+
+    return MeetingParticipationResDTO.from(meetingParticipant);
   }
 }
