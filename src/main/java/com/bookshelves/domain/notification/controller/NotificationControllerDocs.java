fab0de41d120604a50343ee26385e368c72c8def
wldsmdl7 <wldmsdl5835@gmail.com>
2026-07-25 03:35:26 +0900

    feat(notification): 디바이스 토큰 등록 API 구현

--- src/main/java/com/bookshelves/domain/notification/controller/NotificationControllerDocs.java
+++ src/main/java/com/bookshelves/domain/notification/controller/NotificationControllerDocs.java
@@ -1,6 +1,21 @@
 package com.bookshelves.domain.notification.controller;
 
+import com.bookshelves.domain.notification.dto.request.FcmTokenRegisterRequest;
+import com.bookshelves.global.apiPayload.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "알림", description = "알림 API")
-public interface NotificationControllerDocs {}
+public interface NotificationControllerDocs {
+
+  @Operation(
+    summary = "FCM 디바이스 토큰 등록",
+    description = "인증된 사용자의 FCM 토큰을 등록합니다. MVP에서는 플랫폼을 IOS로 저장합니다.")
+  @PostMapping("/api/v1/notifications/fcm/tokens")
+  ResponseEntity<ApiResponse<Void>> registerFcmToken(
+    @Valid @RequestBody FcmTokenRegisterRequest request);
+}
--- src/main/java/com/bookshelves/domain/notification/repository/DeviceTokenRepository.java
+++ src/main/java/com/bookshelves/domain/notification/repository/DeviceTokenRepository.java
@@ -1,6 +1,10 @@
 package com.bookshelves.domain.notification.repository;
 
 import com.bookshelves.domain.notification.entity.DeviceToken;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DeviceTokenRepository extends JpaRepository<DeviceToken, Long> {}
+public interface DeviceTokenRepository extends JpaRepository<DeviceToken, Long> {
+
+  Optional<DeviceToken> findByFcmToken(String fcmToken);
+}
