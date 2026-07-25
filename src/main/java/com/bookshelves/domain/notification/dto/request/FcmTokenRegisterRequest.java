cb2caddfbcb2f4458a655352ffd852e53c3ce6ee
wldsmdl7 <wldmsdl5835@gmail.com>
2026-07-25 09:58:32 +0900

    refactor(notification): 코드 포맷 수정

--- src/main/java/com/bookshelves/domain/notification/dto/request/FcmTokenRegisterRequest.java
+++ src/main/java/com/bookshelves/domain/notification/dto/request/FcmTokenRegisterRequest.java
@@ -5,10 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record FcmTokenRegisterRequest(
-  @Schema(
-    description = "iOS 디바이스에서 발급받은 FCM 등록 토큰",
-    example = "eYx7K9mQ2Vw:APA91bFcmDeviceTokenExample")
-  @NotBlank
-  @Size(max = 255)
-  String fcmToken) {
-}
+    @Schema(
+            description = "iOS 디바이스에서 발급받은 FCM 등록 토큰",
+            example = "eYx7K9mQ2Vw:APA91bFcmDeviceTokenExample")
+        @NotBlank
+        @Size(max = 255)
+        String fcmToken) {}
