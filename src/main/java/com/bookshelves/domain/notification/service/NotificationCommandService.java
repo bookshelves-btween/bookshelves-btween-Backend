a3c5ae64470e7df58a659402ecd80ae54e73faf7
wldsmdl7 <wldmsdl5835@gmail.com>
2026-07-25 11:09:53 +0900

    fix(notification): FCM 토큰 동시 등록을 원자적으로 처리

--- src/main/java/com/bookshelves/domain/notification/service/NotificationCommandService.java
+++ src/main/java/com/bookshelves/domain/notification/service/NotificationCommandService.java
@@ -1,8 +1,5 @@
 package com.bookshelves.domain.notification.service;
 
-import com.bookshelves.domain.member.entity.Member;
-import com.bookshelves.domain.member.repository.MemberRepository;
-import com.bookshelves.domain.notification.entity.DeviceToken;
 import com.bookshelves.domain.notification.repository.DeviceTokenRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,15 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class NotificationCommandService {
 
   private final DeviceTokenRepository deviceTokenRepository;
-  private final MemberRepository memberRepository;
 
   public void registerFcmToken(Long memberId, String fcmToken) {
-    Member member = memberRepository.getReferenceById(memberId);
-
-    deviceTokenRepository
-        .findByFcmToken(fcmToken)
-        .ifPresentOrElse(
-            deviceToken -> deviceToken.assignTo(member),
-            () -> deviceTokenRepository.save(DeviceToken.create(member, fcmToken)));
+    deviceTokenRepository.upsertFcmToken(memberId, fcmToken);
   }
 }
