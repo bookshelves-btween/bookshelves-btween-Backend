9b2f1af11676dcbb6f537d30b74b6a0aff8d98d2
wldsmdl7 <wldmsdl5835@gmail.com>
2026-07-25 11:14:10 +0900

    refactor(common): API 응답 코드 형식 통일

--- src/main/java/com/bookshelves/domain/notification/code/NotificationSuccessCode.java
+++ src/main/java/com/bookshelves/domain/notification/code/NotificationSuccessCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum NotificationSuccessCode implements BaseSuccessCode {
-  FCM_TOKEN_REGISTERED(HttpStatus.OK, "NOTI200", "FCM 토큰이 등록되었습니다.");
+  FCM_TOKEN_REGISTERED(HttpStatus.OK, "NOTI200_1", "FCM 토큰이 등록되었습니다.");
 
   private final HttpStatus status;
   private final String code;
