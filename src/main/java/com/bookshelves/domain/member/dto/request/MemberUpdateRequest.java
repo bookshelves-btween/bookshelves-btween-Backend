4facbf31fd8796a50900e859712833b0d2a666d6
merakyb <bin030922@gmail.com>
2026-08-13 19:22:07 +0900

    docs(api): Swagger 요청 예시 값을 실제 값으로 보완

--- src/main/java/com/bookshelves/domain/member/dto/request/MemberUpdateRequest.java
+++ src/main/java/com/bookshelves/domain/member/dto/request/MemberUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.bookshelves.domain.member.dto.request;
 
 import com.bookshelves.domain.member.enums.ProfileBackgroundColor;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,9 +12,17 @@ import lombok.extern.jackson.Jacksonized;
 @Jacksonized
 public class MemberUpdateRequest {
 
+  @Schema(example = "책")
   private String nicknameNoun;
+
+  @Schema(example = "먹는")
   private String nicknameModifier;
+
+  @Schema(example = "여우")
   private String nicknameAnimal;
+
   private ProfileBackgroundColor profileBackgroundColor;
+
+  @Schema(example = "[1, 2]")
   private List<Long> categoryIds;
 }
